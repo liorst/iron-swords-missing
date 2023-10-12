51bd6a1d287889465898387a8665f21a112aaf1d
Lior Shem Tov <liorst187@gmail.com>
2023-10-12 13:40:32 +0300

    adding 'pending' status filtering for fetching search results
    refactored fetchById implememntion to be seperate from fetchAll to support profile page for 'pending' reports

--- src/actions/fetch-by-id.ts
+++ src/actions/fetch-by-id.ts
@@ -1,9 +1,41 @@
 "use server";
 import { cookies } from "next/headers";
-import { fetchDbData } from "@/services/fetch-db-data";
+import supabase from "@/services/supabase";
+import validator from "validator";
 
 export async function fetchById({ id }: { id: string }) {
   const _cookies = cookies();
-  const data = await fetchDbData({ id });
-  return data[0];
+  let query = `or(id.eq.${validator.escape(id)})`;
+  const { data = [] } = await supabase.from("people").select("*").or(query);
+
+  // @ts-ignore
+  return (
+    data?.map(
+      ({
+        id,
+        contact_name,
+        contact_phone,
+        details,
+        first_name,
+        image,
+        last_name,
+        last_seen,
+        notes,
+        status,
+        source,
+      }) => ({
+        id,
+        contactName: contact_name,
+        contactPhone: contact_phone,
+        identifyingDetails: details,
+        firstName: first_name,
+        image,
+        lastName: last_name,
+        lastSeen: last_seen,
+        notes,
+        status,
+        source,
+      }),
+    ) ?? []
+  );
 }
