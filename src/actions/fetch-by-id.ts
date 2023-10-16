119aea4e0e400fbc463a57c583da4220ea9fc456
Lior Shem Tov <liorst187@gmail.com>
2023-10-16 14:14:19 +0300

    reverting change to fetch-by-id

--- src/actions/fetch-by-id.ts
+++ src/actions/fetch-by-id.ts
@@ -1,38 +1,9 @@
 "use server";
 import { cookies } from "next/headers";
-import supabase from "@/services/supabase-service-role";
-import validator from "validator";
+import { fetchDbData } from "@/services/fetch-db-data";
 
 export async function fetchById({ id }: { id: string }) {
   const _cookies = cookies();
-  let query = `or(id.eq.${validator.escape(id)})`;
-  const { data = [] } = await supabase.from("people").select("*").or(query);
-  // @ts-ignore
-  return (data?.map(
-    ({
-      id,
-      contact_name,
-      contact_phone,
-      details,
-      first_name,
-      image,
-      last_name,
-      last_seen,
-      notes,
-      status,
-      source,
-    }) => ({
-      id,
-      contactName: contact_name,
-      contactPhone: contact_phone,
-      identifyingDetails: details,
-      firstName: first_name,
-      image,
-      lastName: last_name,
-      lastSeen: last_seen,
-      notes,
-      status,
-      source,
-    }),
-  ) ?? [])[0];
+  const data = await fetchDbData({ id });
+  return data[0];
 }
